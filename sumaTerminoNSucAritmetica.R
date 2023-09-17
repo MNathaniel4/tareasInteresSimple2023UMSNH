0636de85b6ed9ea2023ede03d954214efc4e78f8
Nathaniel <143750705+MNathaniel4@users.noreply.github.com>
2023-09-17 12:44:44 -0600

    Add files via upload

---
+++ sumaTerminoNSucAritmetica.R
@@ -0,0 +1,9 @@
+sumaTerminoNSucAritmetica= function(valor_inical,valor_final,n_vez)
+{
+  S_n = (n_vez /2)*(valor_inical+valor_final)
+  
+ print(paste0("La suma de los terminos de la sucesion que inicia en ",
+              valor_inical, " y termina en ",valor_final," que es el termino "
+              ,n_vez," de la susesion es de: ")) 
+ return (S_n)
+}
